e74e6948e4fb2d8e9c52dd36f0557f48af2b5ece
suninformation <suninformation@163.com>
2022-02-28 15:25:12 +0800

    Initial Commit.

---
+++ src/main/java/net/ymate/web/adminlte/AdminlteAssetsUnpacker.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2007-2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.ymate.web.adminlte;
+
+import net.ymate.module.unpack.IUnpack;
+import net.ymate.module.unpack.annotation.Unpack;
+
+/**
+ * @author 刘镇 (suninformation@163.com) on 2021/9/14 11:38 下午
+ * @since 1.0.0
+ */
+@Unpack("web.adminlte.assets")
+public class AdminlteAssetsUnpacker implements IUnpack {
+}
